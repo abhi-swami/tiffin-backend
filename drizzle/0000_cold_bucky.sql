157de3a3862034624f8098cc754784972074426e
abhi-swami <abhi-swami@gmail.com>
2026-03-29 13:27:31 +0530

    update

--- drizzle/0000_cold_bucky.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"first_name" varchar(100) NOT NULL,
-	"last_name" varchar(100) NOT NULL,
-	"mobile_number" varchar(10) NOT NULL,
-	"created_at" "cal::local_datetime" DEFAULT now()
-);
