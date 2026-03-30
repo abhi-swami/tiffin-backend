86202f085bb40dbdd3ebaf59d2ab7aa0676c9d23
abhi-swami <abhi-swami@gmail.com>
2026-03-30 10:26:58 +0530

    update tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,27 +1,10 @@
-import { pgTable, uuid, text, date, integer } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, date, integer, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").defaultRandom().primaryKey(),
   phone: text("phone").notNull().unique(),
-});
-
-export const menus = pgTable("menus", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  date: date("date").notNull().unique(),
-  items: text("items").notNull(), // JSON string
-});
-
-export const orders = pgTable("orders", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  userId: uuid("user_id").notNull(),
-  date: date("date").notNull(),
-  status: text("status").default("pending"),
-  totalAmount: integer("total_amount"),
-});
-
-export const orderItems = pgTable("order_items", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  orderId: uuid("order_id").notNull(),
-  itemName: text("item_name"),
-  quantity: integer("quantity"),
+  email: text("email").unique(),
+  first_name:varchar("first_name",{length:100}).notNull(),
+  last_name:varchar("last_name",{length:100}).notNull(),
+  profile_image:varchar("profile_image",{length:1000})
 });
